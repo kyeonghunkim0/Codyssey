fabfe574d0f21a138fecebc7c068bb993806cf83
KyeongHun Kim <kimkhuna1@gmail.com>
2025-03-16 00:17:14 +0900

    Add 1st project files

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+# 설치가 잘 되었는지 확인 하기 위해서 ‘Hello Mars’를 출력해 본다.
+print("Hello Mars")
+#  log 파일을 열기
+log_data = []
+log = open('mission_computer_main.log', 'r', encoding='utf-8')
+try:
+    next(log)
+    for line in log:
+        parts = line.strip().split(',')
+        log_data.append({
+            'timestamp' : parts[0],
+            'event' : parts[1],
+            'message' : parts[2]
+        })
+finally:
+    log.close()
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# 산소 탱크 관련 이벤트 필터링
+    incident_logs = [entry for entry in log_data if 'Oxygen tank' in entry['message']]
 
+# 사고 원인 분석
+cause = ""
+if any("unstable" in entry['message'] for entry in incident_logs):
+    cause = "Oxygen tank unstable"
+if any("explosion" in entry['message'] for entry in incident_logs):
+    cause = "Oxygen tank explosion"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Markdown 작성
+    markdown_content = f"사고 원인: {cause}\n\n"
 
+# 로그 타임라인 생성(
+    for log in reversed(log_data):
+        markdown_content += f"[{log['timestamp']}] - {log['event']}: {log['message']}\n"
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Markdown 파일로 저장
+    file = open('log_analysis.md', 'w', encoding='utf-8')
+    try:
+        file.write(markdown_content)
+    finally:
+        file.close()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print('Save markdown file success!')
