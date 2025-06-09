5ae3bed34a5993a3ec24c1133843ad46715eead0
KyeongHun Kim <kimkhuna1@gmail.com>
2025-06-09 20:34:58 +0900

    feat: 보너스 문제 추가

--- step12/mars_weather_summary.py
+++ step12/mars_weather_summary.py
@@ -6,30 +6,68 @@ csv_file = 'mars_weathers_data.csv'
 df = pd.read_csv(csv_file)
 print("CSV 데이터 미리보기:\n", df.head())
 
-# 2. MySQL 연결 정보 입력
-conn = pymysql.connect(
-    host='127.0.0.1',
-    user='root',    # 본인의 MySQL 유저명
-    password='root',          # 본인의 MySQL 비밀번호
-    db='sys',                # 데이터베이스 이름 (예시: sys, mars, test 등)
-    charset='utf8'
-)
-cursor = conn.cursor()
+class MySQLHelper:
+    """MySQL 데이터베이스 연결 및 쿼리 실행을 도와주는 클래스"""
 
-# 3. 테이블이 이미 생성되어 있다고 가정 (없으면 위 쿼리 참고해서 생성)
+    def __init__(self, host, user, password, db, charset='utf8'):
+        """초기화: 연결 정보 설정 및 DB 연결"""
+        self.conn = pymysql.connect(
+            host=host,
+            user=user,
+            password=password,
+            db=db,
+            charset=charset
+        )
+        self.cursor = self.conn.cursor()
 
-# 4. 데이터 INSERT
-insert_sql = """
-INSERT INTO mars_weather (mars_date, temp, storm)
-VALUES (%s, %s, %s)
-"""
+    def execute(self, query, params=None):
+        """INSERT, UPDATE, DELETE 등 쿼리 실행"""
+        self.cursor.execute(query, params or ())
+        self.conn.commit()
 
-for idx, row in df.iterrows():
-    cursor.execute(insert_sql, (row['mars_date'], int(row['temp']), int(row['storm'])))
+    def fetchall(self, query, params=None):
+        """SELECT 쿼리 실행 후 모든 결과 반환"""
+        self.cursor.execute(query, params or ())
+        return self.cursor.fetchall()
 
-conn.commit()
-print(f"{cursor.rowcount}개 레코드가 삽입되었습니다.")
+    def fetchone(self, query, params=None):
+        """SELECT 쿼리 실행 후 한 개 결과 반환"""
+        self.cursor.execute(query, params or ())
+        return self.cursor.fetchone()
 
-# 5. 연결 종료
-cursor.close()
-conn.close()
+    def close(self):
+        """DB 연결 종료"""
+        self.cursor.close()
+        self.conn.close()
+
+
+# 사용 예시
+if __name__ == '__main__':
+    # 연결 정보 입력
+    db_helper = MySQLHelper(
+        host='127.0.0.1',
+        user='root',  # 본인의 MySQL 유저명
+        password='root',  # 본인의 MySQL 비밀번호
+        db='sys',  # 데이터베이스 이름 (예시: sys, mars, test 등)
+        charset='utf8'
+    )
+
+    # SELECT
+    results = db_helper.fetchall("SELECT * FROM mars_weather;")
+    for row in results:
+        print(row)
+
+    # INSERT
+    # insert_sql = """
+    #     INSERT INTO mars_weather (mars_date, temp, storm)
+    #     VALUES (%s, %s, %s)
+    # """
+    #
+    # for _, row in df.iterrows():
+    #     db_helper.execute(insert_sql, (row['mars_date'], int(row['temp']), int(row['storm'])))
+
+    # DELETE
+    # db_helper.execute("TRUNCATE TABLE mars_weather;")
+
+    db_helper.close()
+
