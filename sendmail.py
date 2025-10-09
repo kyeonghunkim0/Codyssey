916466317fc02e932b115c4142a1d30d2aa883f3
kyeonghumkim0 <kimkhuna1@gmail.com>
2025-10-10 00:10:06 +0900

    feat: 5주차 보너스 과제 추가

--- sendmail.py
+++ sendmail.py
@@ -1,17 +1,39 @@
 # 라이브러리 불러오기
+
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
 # 개인 정보 입력(email, 앱 비밀번호)
 my_email = "kimkhuna@dongyang.ac.kr"
 password = "PASSWORD"
 
-# 방법 1(with 사용 X)
-connection = smtplib.SMTP("smtp.gmail.com") #보내는 메일의 SMTP 주소입력
-connection.starttls() # Transport Layer Security : 메시지 암호화 기능
+
+# 메일 구성
+msg = MIMEMultipart()
+msg["From"] = my_email
+msg["To"] = "kimkhuna@naver.com"
+msg["Subject"] = "제목제목제목"
+
+# 본문 추가
+body = "내용내용내용"
+msg.attach(MIMEText(body, "plain"))
+
+# 첨부 파일 추가
+filename = "index.html"  # 첨부할 파일명
+with open(filename, "rb") as attachment:
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload(attachment.read())
+
+encoders.encode_base64(part)
+part.add_header("Content-Disposition", f"attachment; filename={filename}")
+msg.attach(part)
+
+# SMTP 설정 및 전송
+connection = smtplib.SMTP("smtp.gmail.com", 587)
+connection.starttls()
 connection.login(user=my_email, password=password)
-connection.sendmail(
-	from_addr=my_email,
-    to_addrs="kimkhuna@naver.com",
-    msg="Subject:Hello\n\nThis is the body of my email."
-)
+connection.sendmail(my_email, "kimkhuna@naver.com", msg.as_string())
 connection.close()
