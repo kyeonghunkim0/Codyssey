97ca31ab3b7dfd6ef9dcb1727d0e876cc2d2ffc1
kyeonghumkim0 <kimkhuna1@gmail.com>
2025-11-03 23:35:07 +0900

    feat: 9주차 과제 추가

---
+++ todo.py
@@ -0,0 +1,65 @@
+# todo.py
+
+# python3 -m venv venv
+# source .venv/bin/activate
+# pip install "fastapi[all]"
+
+# GET curl -X GET "http://127.0.0.1:8000/todos"
+
+# POST curl -X POST 'http://127.0.0.1:8000/todos' \
+# -H 'Content-Type: application/json' \
+# -d '{"task": "이건 급한 일이야", "priority": "high"}'
+
+# curl -X POST 'http://127.0.0.1:8000/todos' \
+# -H 'Content-Type: application/json' \
+# -d '{}'
+
+
+from fastapi import FastAPI, APIRouter, HTTPException, status
+from typing import Dict, List, Any
+
+#  리스트 객체를 todo_list라는 이름으로 추가
+todo_list: List[Dict[str, Any]] = []
+
+# APIRouter 클래스를 이용해서 라우트 추가
+router = APIRouter()
+
+
+# (수행과제 7) add_todo ()
+# POST 방식, todo_list에 항목 추가, 입출력은 Dict
+# (제약사항 C8) 함수 이름은 소문자와 언더라인 사용
+@router.post('/todos')
+def add_todo(todo: Dict[str, Any]):
+    """
+    새로운 할 일(todo)을 todo_list에 추가합니다.
+    입력값은 Dict 타입입니다.
+    """
+
+    # (보너스 과제) 입력되는 Dict 타입이 빈값이면 400 에러.
+    is_empty = (not todo)
+    if is_empty:
+
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Input dictionary cannot be empty'
+        )
+
+    todo_list.append(todo)
+
+    # 입출력은 Dict 타입
+    return {'message': 'Todo added successfully'}
+
+
+# (수행과제 8) retrieve_todo()
+# GET 방식, todo_list를 가져옴
+@router.get('/todos')
+def retrieve_todo():
+    """
+    전체 todo_list를 반환합니다.
+    """
+    return todo_list
+
+
+# FastAPI 앱 생성 및 라우터 포함
+app = FastAPI()
+app.include_router(router)
